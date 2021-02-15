f925ad580e36e142c9829e9fb897293ae5291375
NellyAnda <nellyanda18@gmail.com>
2021-02-15 23:17:07 +0100

    small style changes on the registration page and a few blades added

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -25,4 +25,12 @@ class HomeController extends Controller
     {
         return view('home');
     }
+
+    public function acupuncture()
+    {
+        return view('acupuncture');
+    }
+
+    
+
 }
